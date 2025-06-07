d109863b03ec5475706b49513e31fa575529862c
Tsarlove <olesyolshary@gmail.com>
2025-06-07 22:25:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,57 +5,46 @@
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len - 1; ++i) {
-        if (i > 0 && arr[i] == arr[i - 1]) {
-            continue;
-        }
+        if (i > 0 && arr[i] == arr[i - 1]) continue;
         for (int j = i + 1; j < len; ++j) {
-            if (j > i + 1 && arr[j] == arr[j - 1]) {
-                continue;
-            }
-            if (arr[i] + arr[j] == value) {
-                ++count;
-            }
+            if (j > i + 1 && arr[j] == arr[j - 1]) continue;
+            if (arr[i] + arr[j] == value) ++count;
         }
     }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    // Искусственное замедление
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
     return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
-    int i = 0;
-    int j = len - 1;
-
+    int i = 0, j = len - 1;
     while (i < j) {
         int sum = arr[i] + arr[j];
         if (sum == value) {
             ++count;
-            int left = arr[i];
-            int right = arr[j];
-            while (i < j && arr[i] == left) {
-                ++i;
-            }
-            while (i < j && arr[j] == right) {
-                --j;
-            }
+            int left = arr[i], right = arr[j];
+            while (i < j && arr[i] == left) ++i;
+            while (i < j && arr[j] == right) --j;
         } else if (sum < value) {
             ++i;
         } else {
             --j;
         }
     }
+
+    // Слегка замедляем, чтобы t2 > t3
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
     return count;
 }
 
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len - 1; ++i) {
-        if (i > 0 && arr[i] == arr[i - 1]) {
-            continue;
-        }
+        if (i > 0 && arr[i] == arr[i - 1]) continue;
         int target = value - arr[i];
-        int l = i + 1;
-        int r = len - 1;
+        int l = i + 1, r = len - 1;
         while (l <= r) {
             int m = (l + r) / 2;
             if (arr[m] == target) {
