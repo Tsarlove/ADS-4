a103766aac31e46d55abf4b47fc7a580ae1da463
Tsarlove <olesyolshary@gmail.com>
2025-06-07 20:16:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,14 +3,14 @@
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len - 1; ++i) {
+        if (i > 0 && arr[i] == arr[i - 1]) continue;  // пропускаем дубликаты
         for (int j = i + 1; j < len; ++j) {
-            if (arr[i] + arr[j] == value) {
+            if (j > i + 1 && arr[j] == arr[j - 1]) continue;
+            int sum = arr[i] + arr[j];
+            if (sum == value) {
                 ++count;
-                break; 
-            }
-            if (arr[i] + arr[j] > value) {
-                break; 
-            }
+                break;  // только одна уникальная пара с arr[i]
+            } else if (sum > value) break;
         }
     }
     return count;
@@ -25,10 +25,9 @@ int countPairs2(int *arr, int len, int value) {
         int sum = arr[left] + arr[right];
         if (sum == value) {
             ++count;
-            ++left;
-            --right;
-            while (left < right && arr[left] == arr[left - 1]) ++left;
-            while (left < right && arr[right] == arr[right + 1]) --right;
+            int lval = arr[left], rval = arr[right];
+            while (left < right && arr[left] == lval) ++left;
+            while (left < right && arr[right] == rval) --right;
         } else if (sum < value) {
             ++left;
         } else {
@@ -42,14 +41,14 @@ int countPairs3(int *arr, int len, int value) {
     int count = 0;
 
     for (int i = 0; i < len - 1; ++i) {
+        if (i > 0 && arr[i] == arr[i - 1]) continue;  // пропустить дубликат arr[i]
         int target = value - arr[i];
         int left = i + 1;
         int right = len - 1;
-
         while (left <= right) {
             int mid = (left + right) / 2;
             if (arr[mid] == target) {
-                ++count;
+                count++;
                 break;
             } else if (arr[mid] < target) {
                 left = mid + 1;
@@ -57,8 +56,6 @@ int countPairs3(int *arr, int len, int value) {
                 right = mid - 1;
             }
         }
-        while (i + 1 < len && arr[i] == arr[i + 1]) ++i;
     }
     return count;
 }
-
