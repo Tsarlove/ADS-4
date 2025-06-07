638fcdc69f979f358491baabf0b93c671ced6019
Tsarlove <olesyolshary@gmail.com>
2025-06-07 21:37:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -25,36 +25,20 @@ int countPairs2(int *arr, int len, int value) {
         int sum = arr[left] + arr[right];
         if (sum == value) {
             ++count;
-            int lval = arr[left], rval = arr[right];
-            while (left < right && arr[left] == lval) ++left;
-            while (left < right && arr[right] == rval) --right;
+
+            int lval = arr[left];
+            int rval = arr[right];
+
+            while (left < right && arr[left] == lval) {
+                ++left;
+            }
+            while (left < right && arr[right] == rval) {
+                --right;
+            }
         } else if (sum < value) {
             ++left;
         } else {
             --right;
-        }
-    }
-    return count;
-}
-
-int countPairs3(int *arr, int len, int value) {
-    int count = 0;
-
-    for (int i = 0; i < len - 1; ++i) {
-        if (i > 0 && arr[i] == arr[i - 1]) continue;  // пропустить дубликат arr[i]
-        int target = value - arr[i];
-        int left = i + 1;
-        int right = len - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] == target) {
-                count++;
-                break;
-            } else if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
         }
     }
     return count;
